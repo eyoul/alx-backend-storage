b75d83b53b2bc57e0d71b7f06d50d10e8cc11845
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-04-06 12:58:37 -0700

    Update 6-bonus.sql

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -2,16 +2,15 @@
 -- that adds a new correction for a student.
 DELIMITER$$;
 CREATE PROCEDURE AddBonus(
-    IN user_id INTEGER,
-    IN project_name VARCHAR(255),
-    IN score INTEGER
+        IN user_id INTEGER,
+        IN project_name VARCHAR(255),
+        IN score INTEGER
 )
 BEGIN
-    IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
-        INSERT INTO projects (name) VALUES (projects_name);
-    END IF;
-    INSERT INTO corrections (user_id, project_id, score)
-    VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
-    
+        IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
+            INSERT INTO projects (name) VALUES (projects_name);
+        END IF;
+        INSERT INTO corrections (user_id, project_id, score)
+        VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
 END;$$
 DELIMITER;
