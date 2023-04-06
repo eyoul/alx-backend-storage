11e70dd3a3cc9f71c60f49a4d016de018a710819
eyoul <eyoulshimeles2@gmail.com>
2023-04-06 18:52:46 +0000

    3-glam_rock.sql update

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
--- SQL script that lists all bands with Glam rock as their main style
--- ranked by thier longevity (number of years they have been active)
-SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
-FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', style) > 0
-ORDER BY lifespan DESC;
+--  creates a trigger that decreases the quantity of an item after adding
+--  Quantity in the table items can be negative.
+
+CREATE TRIGGER decrease_q AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
