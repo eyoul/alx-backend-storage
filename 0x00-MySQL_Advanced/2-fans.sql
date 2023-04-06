5b937adc156f978bec42936fca879d5388c84637
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-04-06 06:23:37 -0700

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
 -- Column names must be: origin and nb_fans
 -- Your script can be executed on any database
-SELECT origin, SUM(fans) as  from metal_bands
+SELECT origin, SUM(fans) as nb_fans from metal_bands
 GROUP BY origin ORDER BY nb_fans DESC;
