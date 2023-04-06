bafb6776ce2fd07393375bb801997247280143ce
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-04-06 10:59:59 -0700

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,4 +2,4 @@
 -- attributes formed and split for computing the lifespan
 
 SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM
-metal_bands WHERE style LIKE '%Glam rock%'ORDER BY lifespan DESC;
+metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
