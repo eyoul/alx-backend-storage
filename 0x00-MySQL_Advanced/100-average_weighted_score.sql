a5fb759af080871dd0de01435f50edce6f0a3032
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-04-06 14:51:54 -0700

    Update 100-average_weighted_score.sql

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,7 +1,7 @@
 -- SQL script that creates a stored procedure
 -- ComputeAverageWeightedScoreForUser that computes
 -- and store the average weighted score for a student.
-ROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
 BEGIN
