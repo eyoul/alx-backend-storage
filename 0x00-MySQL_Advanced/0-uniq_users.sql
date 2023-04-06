df3b398a91ec084a78658433168b01f047255442
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-04-06 05:07:44 -0700

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- SQL script that creates a table users following these 
 -- id, email, name
-CREATE TABLE if NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS user (
         id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
         email varchar(255) NOT NULL UNIQUE,
         name varchar(255)
