03530621429a4c08232c020eac22547e1e7dfaab
eyoul <49184150+eyoul@users.noreply.github.com>
2023-04-14 22:55:12 +0300

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -25,3 +25,95 @@ def count_calls(method: Callable) -> Callable:
             :param self:, args, kwargs
         """
     return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+        Reading from Redis and recovering original type
+    """
+    key = method.__qualname__
+    i = "".join([key, ":inpute"])
+    o = "".join([key, ":outputs"])
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ wrapp """
+        self._redis.rpush(i, str(args))
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(o, str(res))
+        return res
+
+    return wrappers
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    add its input parameters to one list
+    in redis, and store its output into another list.
+    :param method:
+    :return:
+    """
+    key = method.__qualname__
+    i = "".join([key, ":inputs"])
+    o = "".join([key, ":outputs"])
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapp """
+        self._redis.rpush(i, str(args))
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(o, str(res))
+        return res
+
+    return wrapper
+
+
+class Cache:
+    """
+    Cache redis class
+    """
+
+    def __init__(self):
+        """
+        constructor of the redis model
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @count_calls
+    @call_history
+    def store(self, data: UnionOfTypes) -> str:
+        """
+        generate a random key (e.g. using uuid),
+         store the input data in Redis using the
+          random key and return the key.
+        :param data:
+        :return:
+        """
+        key = str(uuid4())
+        self._redis.mset({key: data})
+        return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) \
+            -> UnionOfTypes:
+        """
+        convert the data back
+        to the desired format
+        :param key:
+        :param fn:
+        :return:
+        """
+        if fn:
+            return fn(self._redis.get(key))
+        data = self._redis.get(key)
+        return data
+
+    def get_int(self: bytes) -> int:
+        """get a number"""
+        return int.from_bytes(self, sys.byteorder)
+
+    def get_str(self: bytes) -> str:
+        """get a string"""
+        return self.decode("utf-8")
+Footer
+
