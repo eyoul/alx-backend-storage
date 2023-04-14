1188aba44bdea0868400ce2c43b71c7d3e4a2b31
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-04-15 00:29:25 +0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python3
-import requests
-import time
+""" Module for Implementing an expiring web cache and tracker """
+
 from functools import wraps
-from typing import Dict
+import redis
+import requests
+from typing import Callable
 
-cache: Dict[str, str] = {}
+reddiss = redis.Redis()
+"""reddis client"""
 
 
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting how many times a request
+    has been made """
+
+    @wraps(method)
+    def wrapper(url):
+        """ Wrapper for decorator functionality """
+        reddiss.incr(f"count:{url}")
+        cached_html = reddiss.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+
+        html = method(url)
+        reddiss.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
+
+
+@count_requests
 def get_page(url: str) -> str:
-    if url in cache:
-        print(f"Retrieving from cache: {url}")
-        return cache[url]
-    else:
-        print(f"Retrieving from web: {url}")
-        response = requests.get(url)
-        result = response.text
-        cache[url] = result
-        return result
-
-
-def cache_with_expiration(expiration: int):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            url = args[0]
-            key = f"count:{url}"
-            if key in cache:
-                count, timestamp = cache[key]
-                if time.time() - timestamp > expiration:
-                    result = func(*args, **kwargs)
-                    cache[key] = (count+1, time.time())
-                    return result
-                else:
-                    cache[key] = (count+1, timestamp)
-                    return
+    """Uses the requests module to obtain the HTML
+    content of a particular URL and returns it.
+    """
+    req = requests.get(url)
+    return req.text
