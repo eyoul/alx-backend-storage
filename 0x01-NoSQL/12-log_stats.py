4cc7947c8c06a5cbd9800fc3e08b98b859430d2f
eyoul <49184150+eyoul@users.noreply.github.com>
2023-04-14 22:10:50 +0300

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
-""" lists all documents with name starting by Holberton"""
+""" Nginx logs stored in MongoDB
+"""
+
 
 from pymongo import MongoClient
 
 
 def log_stats():
+    """ Database: logs
     """
-    Display log
-    """
-    client = MongoClient('mogodb://127.0.0.1:27017')
-    log_collection = client.logs.nginx
-    total = log_collection.count_documents({})
-    get = log_collection.count_documents({"method": "GET"})
-    post = log_collection.count_documents({"method": "POST"})
-    put = log_collection.count_documents({"method": "PUT"})
-    patch = log_collection.count_documents({"method": "PATCH"})
-    delete = log_collection.count_documents({"method": "DELETE"})
-    path = log_collection.count_documents({"method": "GET", "path": "/status"})
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    total = logs_collection.count_documents({})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    path = logs_collection.count_documents(
+        {"method": "GET", "path": "/status"})
     print(f"{total} logs")
     print("Methods:")
     print(f"\tmethod GET: {get}")
